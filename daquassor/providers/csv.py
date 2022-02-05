5ae5bd9f86b9e0cfe58555e610e6fa7ac81866b9
SzymonPucher <sajmontube+gitlab@gmail.com>
2022-02-05 08:32:54 +0100

    feat: Add pydantic to csv provider

--- daquassor/providers/csv.py
+++ daquassor/providers/csv.py
@@ -1,13 +1,13 @@
-from daquassor.providers.i_provider import IProvider
+from typing import Optional
+#from daquassor.providers.i_provider import IProvider
 import pandas as pd
+from pydantic import BaseModel, StrictStr
 
 
-class CsvProvider(IProvider):
-    def __init__(self, file_path: str, delimiter: str = ',', quote_char: str = '"') -> None:
-        super().__init__()
-        self.file_path = file_path
-        self.delimiter = delimiter
-        self.quote_char = quote_char
+class CsvProvider(BaseModel):
+    file_path: StrictStr
+    delimiter: Optional[str] = ','
+    quote_char: Optional[str] = '"'
 
     def connect(self) -> None:
         pass
--- tests/integration/test_csv_provider.py
+++ tests/integration/test_csv_provider.py
@@ -1,10 +1,32 @@
+from pydantic import ValidationError
+import pytest
 from daquassor.providers.csv import CsvProvider
 import pandas as pd
 
 
+file_path = 'tests/integration/data/example.csv'
+
+@pytest.mark.parametrize('file_path, delimiter, quote_char, expected_error', [
+    ('file_path_1.csv', ',', '"', None),
+    (pd.DataFrame(), ',', '"', ValidationError),
+    ('file_path_1.csv', [','], '"', ValidationError),
+    ('file_path_1.csv', ',', ('"', ), ValidationError),
+    (123, ',', '"', ValidationError),
+    ('file_path_1.csv', 37, '"', None),  # numeric value can potentially be a delimiter, although it's confusing
+    ('file_path_1.csv', ',', 6543, None)  # same goes for quote_char
+])
+def test_inititation(file_path, delimiter, quote_char, expected_error):
+    # ACT
+    if expected_error:
+        with pytest.raises(expected_error):
+            CsvProvider(file_path=file_path, delimiter=delimiter, quote_char=quote_char)
+    else:
+        CsvProvider(file_path=file_path, delimiter=delimiter, quote_char=quote_char)
+
+
 def test_reading_csv():
     # ARRANGE
-    csv_provider = CsvProvider('tests/integration/data/example.csv')
+    csv_provider = CsvProvider(file_path=file_path)
 
     # ACT
     data = csv_provider.get_data()
@@ -16,7 +38,7 @@ def test_reading_csv():
 
 def test_writing_csv():
     # ARRANGE
-    csv_provider = CsvProvider('tests/integration/data/example.csv')
+    csv_provider = CsvProvider(file_path=file_path)
     example = pd.DataFrame({'name': {0: 'Mark', 1: 'James'}, 'age': {0: 22, 1: 30}})
 
     # ACT
